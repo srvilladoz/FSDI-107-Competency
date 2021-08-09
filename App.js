c2d2ec7abccca6feeb277b9fbffd66dd3bb2b3ad
srvilladoz <85268801+srvilladoz@users.noreply.github.com>
2021-08-08 20:36:59 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
+import Catalog from "./components/catalog";
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "font-awesome/css/font-awesome.min.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+
+      <Catalog></Catalog>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
